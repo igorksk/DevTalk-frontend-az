732f88ddf7177f25adc08280d08e5a147c1800ac
Igor Kaseko <igorksk@gmail.com>
2026-05-19 21:00:04 +0300

    fix: update baseURL for API client to point to production backend

--- src/api/client.ts
+++ src/api/client.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // All API calls go through this configured Axios instance
 // In development, Vite's proxy forwards /api/* to http://localhost:5000
 const apiClient = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://devtalk-backend-afftbndvehgzfqah.canadaeast-01.azurewebsites.net/api',
   headers: {
     'Content-Type': 'application/json',
   },
